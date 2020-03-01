c1ca38fc9140f49e65f6ab2a6c0d8ba6c9770877
s16865 <s16865@LAB130-11>
2020-03-01 19:38:54 +0100

    Zadanie 4

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -10,17 +10,36 @@ namespace Cw1
     {
         static async Task Main(string[] args)
         {
+
+            // wymaganie 1
+            if (args.Length != 1 || args[0] == "")
+                throw new ArgumentNullException("ArgumentNullException message");
+
+            // wymaganie 2
+            if (!Uri.IsWellFormedUriString(args[0], UriKind.Absolute))
+                throw new ArgumentException("ArgumentException message");
+
+           
             var emails = await getEmails(args[0]);
-            foreach (var a in args)
-            {
-                Console.WriteLine(a);
-            }
 
-            foreach (var email in emails)
+            // wymaganie 5
+            if (emails.Count == 0)
             {
-                Console.WriteLine(email);
-            }
+                Console.WriteLine("Nie znaleziono adresów e-mail");
+            } else
+            {
+                foreach (var a in args)
+                {
+                    Console.WriteLine(a);
+                }
 
+                // wymaganie 6
+                var unique_items = new HashSet<string>(emails);
+                foreach (var email in unique_items)
+                {
+                    Console.WriteLine(email);
+                }
+            }
         }
 
         static async Task<IList<string>> getEmails(string url)
@@ -28,7 +47,22 @@ namespace Cw1
             var httpClient = new HttpClient();
             var listOfEmails = new List<string>();
 
-            var response = await httpClient.GetAsync(url);
+            // wmaganie 4
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                response = await httpClient.GetAsync(url);
+
+                // wymaganie 3
+                httpClient.Dispose();
+            } catch (Exception )
+            {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+
+                // wymaganie 3
+                httpClient.Dispose();
+                return listOfEmails;
+            }
 
             Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
             RegexOptions.IgnoreCase);
@@ -39,6 +73,8 @@ namespace Cw1
             {
                 listOfEmails.Add(emailMatch.ToString());
             }
+
+
 
             return listOfEmails;
 
