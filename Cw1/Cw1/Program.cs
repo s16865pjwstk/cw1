90ab0c629cfc513a683fc9b1a9ffa7e01ab1bf5c
Adam Jatkowski <s16865@pjwstk.edu.pl>
2020-03-01 19:08:12 +0100

    Work in progress

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var emails = await getEmails(args[0]);
+            foreach (var a in args)
+            {
+                Console.WriteLine(a);
+            }
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+
         }
+
+        static async Task<IList<string>> getEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+
+            var response = await httpClient.GetAsync(url);
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            RegexOptions.IgnoreCase);
+
+            MatchCollection emailmatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+            foreach (var emailMatch in emailmatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+
+            return listOfEmails;
+
+        }
+
     }
+
+
 }
